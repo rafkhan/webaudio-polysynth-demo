732e9b7a226c25792a1a28c18f71761d56a82e6b
Rafael Khan <raf.khan.f@gmail.com>
2019-06-14 23:28:55 -0400

    Test 2 oscs

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ function playTwoSounds() {
     audioCtx.resume(); // has to receive input from user to start
 
     const osc = new Oscillator(audioCtx, audioCtx.destination);
+    const osc2 = new Oscillator(audioCtx, audioCtx.destination);
 
     // lmao callback hell
-    osc.playFrequency(400);
+    osc.playFrequency(1000);
+    osc2.playFrequency(200);
     setTimeout(() => {
-        osc.playFrequency(500);
+        osc.playFrequency(1300);
+        osc2.playFrequency(250);
         setTimeout(() => {
             osc.stop();
+            osc2.stop();
         }, 2000);
     }, 2000);
 }
