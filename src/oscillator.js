b29011b7fdb622a469b1c0ecbf740653bfad14c0
Rafael Khan <raf.khan.f@gmail.com>
2019-06-14 23:26:02 -0400

    lil cleanup

--- src/oscillator.js
+++ src/oscillator.js
@@ -2,21 +2,33 @@ export default class Oscillator {
     constructor(audioCtx, destinationNode) {
         this.audioCtx = audioCtx;
         this.oscNode = audioCtx.createOscillator();
-        this.gainNode = audioCtx.createGain();
 
+        // We use a gain node to "start" and "stop" the track
+        // This is basically achieved by muting and unmuting the gain node that sits
+        // infront of the oscillator.
+        this.gainNode = audioCtx.createGain(); 
         this.oscNode.connect(this.gainNode);
         this.gainNode.connect(destinationNode);
 
-        this.gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+        this.mute();
         this.oscNode.start();
+    }
+
+    mute() {
+        this.gainNode.gain.setValueAtTime(0, this.audioCtx.currentTime);
+    }
+
+    unmute() {
+        this.gainNode.gain.setValueAtTime(1, this.audioCtx.currentTime);
     }
 
     playFrequency(hertz) {
         this.oscNode.frequency.setValueAtTime(hertz, this.audioCtx.currentTime); // value in hertz
-        this.gainNode.gain.setValueAtTime(1, this.audioCtx.currentTime);
+        this.unmute();
     }
 
+    // just a nice alias, you logically don't mute an oscillator.
     stop() {
-        this.gainNode.gain.setValueAtTime(0, this.audioCtx.currentTime);
+        this.mute();
     }
 }
